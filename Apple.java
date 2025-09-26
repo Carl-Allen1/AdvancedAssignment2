4b088d57ad77f9dc4f15233f027ca829ebd4ed7b
NinjagoJay7299 <attilabloom7299@gmail.com>
2025-09-26 15:29:27 -0400

    Completed assignment 2

--- Apple.java
+++ Apple.java
@@ -1,11 +1,23 @@
 public class Apple {
-    String color;
-    double size;
+    private String color;
+    private double size;
 
     public Apple() {}
 
     public Apple(String color, double size) {
         this.color = color;
         this.size = size;
+    }
+    public String getColor(){
+        return color;
+    }
+    public double getSize(){
+        return size;
+    }
+    public void setColor(String newColor){
+        color = newColor;
+    }
+    public void setSize(double newSize){
+        size = newSize;
     }
 }
